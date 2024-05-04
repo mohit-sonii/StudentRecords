f2a9022d5c0ff72e64975acecd8c1f7d8cefa6a3
Mohit Soni <mohitsoni9731@gmail.com>
2024-05-04 17:24:58 +0530

    new

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
-const CompassURL = 'mongodb://localhost:27017/StudentRecord'
-// const CompassURL = 'mongodb+srv://mohitsoni:MohitSoni123!!!@studentrecord.6lgj57e.mongodb.net/'
+// const CompassURL = 'mongodb://localhost:27017/StudentRecord'
+const CompassURL = 'mongodb+srv://mohitsoni:MohitSoni123!!!@studentrecord.6lgj57e.mongodb.net/'
 mongoose.connect(CompassURL)
 
 const database = mongoose.connection
